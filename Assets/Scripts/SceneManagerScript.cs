d7181a1302f92507d6881fd010c751d415c27f48
Caypoh23 <caypoh23@gmail.com>
2020-10-27 09:04:11 +0500

    Trying to use singleton

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void GoToMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void GoToRegistrationScene()
+    {
+        SceneManager.LoadScene("Registration");
+    }
+    public void GoToHomeworkScene()
+    {
+        SceneManager.LoadScene("HistoryHomework");
+    }
+    public void GoToSelfWorkScene()
+    {
+        SceneManager.LoadScene("Selftwork");
+    }
+    public void GoToSettingsScene()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+}
