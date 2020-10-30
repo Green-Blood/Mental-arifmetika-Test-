031ba352d083ce53f4cbd34253b650c1b877fe5c
Caypoh23 <caypoh23@gmail.com>
2020-10-30 07:29:22 +0500

    Minor changes to the main scene

---
+++ Assets/Scripts/PanelSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelSwitcher : MonoBehaviour
+{
+    [SerializeField] private GameObject[] menuPanels;
+
+    public void MenuSwapper(int id)
+    {
+        foreach (GameObject menu in menuPanels)
+        {
+            menu.SetActive(false);
+        }
+        menuPanels[id].SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UsernamePusher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UsernamePusher : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI _usernameRegistration;
+    [SerializeField] private TextMeshProUGUI _usernameMainMenu;
+
+    public void SetTextOnUser()
+    {
+        _usernameMainMenu.text = _usernameRegistration.text ;
+    }
+
+
+}
