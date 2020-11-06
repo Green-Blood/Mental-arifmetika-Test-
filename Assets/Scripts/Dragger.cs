79bf30376c3955d4f16109a49c9b1a342e2b2d67
Caypoh23 <caypoh23@gmail.com>
2020-11-06 19:39:37 +0500

    Minor refactoring to the main scene

---
+++ Assets/Scripts/Dragger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dragger : MonoBehaviour
+{
+    [SerializeField] private RectMask2D _maskTree;
+    [SerializeField] private RectMask2D _mask;
+
+
+    public void OnMouseDrag()
+    {
+        Debug.Log("Alo ya tut");
+        _maskTree.enabled = true;
+        _mask.enabled = false;
+
+    }
+    public void OnMouseUp()
+    {
+
+        _maskTree.enabled = false;
+        _mask.enabled = true;
+    }
+}
--- Assets/Scripts/PanelSwitcher.cs
+++ Assets/Scripts/PanelSwitcher.cs
@@ -1,4 +1,5 @@
 ﻿
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,20 +10,41 @@ public class PanelSwitcher : MonoBehaviour
     [SerializeField] private GameObject[] menuPanels;
     [SerializeField] private Animator _menuAnimator;
     [SerializeField] private MenuAnimations _switchAnimation;
-
+    [SerializeField] private float waitTime;
     public void MenuSwapper(int id)
     {
+         
         if (SoundManager.Instance.soundManager.volume > 0) 
         {
             SoundManager.Instance.PlaySound();
         }
-
+        StartCoroutine(ScaleCouroutine(waitTime, id));
         foreach (GameObject menu in menuPanels)
         {
-            menu.SetActive(false);
+            menu.transform.DOScale(0, waitTime);
         }
-        _switchAnimation.ScaleDownMenu(true);
+        menuPanels[id].SetActive(true);
+        menuPanels[id].transform.DOScale(1, waitTime);
+        /*foreach (GameObject menu in menuPanels)
+        {
+            menu.SetActive(false);
+        }*/
+
+        //_switchAnimation.ScaleDownMenu(true);
+
+       
+    }
+
+    private IEnumerator ScaleCouroutine(float time, int id)
+    {
+        yield return new WaitForSeconds(time);
+        menuPanels[id].transform.DOScale(1, time);
         menuPanels[id].SetActive(true);
     }
+
+   
+    
+
+
 
 }
